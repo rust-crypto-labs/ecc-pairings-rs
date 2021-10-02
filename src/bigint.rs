5ae135dd0e0c39bb6a8ef43e167d1f7736c2496d
redarkyan <romain.ninot@outlook.fr>
2021-10-02 17:22:27 +0200

    Implement Structs

--- src/bigint.rs
+++ src/bigint.rs
@@ -1,5 +1,10 @@
+use std::ops::Mul;
+use std::ops::Add;
+
 // Big number (to be replaced by an actual bignum library)
 // Must support negative integers
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct BigInt {}
 
 // Example bignum operations
@@ -48,6 +53,31 @@ impl BigInt {
     // Returns the binary representation of self, starting
     // with the least-significant bits
     pub fn to_bits(self) -> Vec<bool> {
+        unimplemented!()
+    }
+
+    pub fn modulo(self, y: &u32) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Mul<BigInt> for BigInt {
+    type Output = BigInt;
+    fn mul(self, rhs: BigInt) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl Mul<i64> for BigInt {
+    type Output = BigInt;
+    fn mul(self, rhs: i64) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+impl Add<BigInt> for BigInt {
+    type Output = BigInt;
+    fn add(self, rhs: BigInt) -> Self::Output {
         unimplemented!()
     }
 }
--- src/field.rs
+++ src/field.rs
@@ -2,10 +2,10 @@ use crate::bigint::BigInt;
 // Generic finite field operations
 pub trait Field {
     // Neutral element for addition
-    fn zero() -> Self;
+    fn zero(self) -> Self;
 
     // Neutral element for multiplication
-    fn one() -> Self;
+    fn one(self) -> Self;
 
     // Addition
     fn add(self, y: &Self) -> Self;
@@ -14,7 +14,7 @@ pub trait Field {
     fn mul(self, y: &Self) -> Self;
 
     // Multiplication by an integer
-    fn zmul(self, y: i64) -> Self;
+    fn zmul(self, y: &i64) -> Self;
 
     // Power
     fn pow(self, y: &BigInt) -> Self;
@@ -32,16 +32,16 @@ pub trait Field {
     fn sqrt(self) -> Self;
 
     // Multiplicative inverse
-    fn invert(self) -> Self;
+    fn invert(&self) -> Self;
 
     // Additive inverse
     fn neg(self) -> Self;
 
     // Degree of the extension
-    fn degree() -> Self;
+    fn degree() -> u32;
 
     // Field order
-    fn order() -> Self;
+    fn order(self) -> u32;
 
     // Base field order
     fn base_order() -> BigInt;
@@ -50,49 +50,52 @@ pub trait Field {
     fn random_element() -> Self;
 }
 
-pub enum Scalar<F:Field + Clone> {
-    PFScalar(PrimeField),
-    FFScalar(FiniteField),
+pub enum Scalar<const P:u32, const N:u32> {
+    PFScalar(PrimeField<P>),
+    FFScalar(FiniteField<P,N>),
 }
-pub struct PrimeField {
-    pub order: i32,
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct PrimeField<const P:u32> {
     pub value: BigInt,
 }
 
-pub struct FiniteField {
-    pub order: i32,
-    pub degree: i32,
-    pub coords: Vec<PrimeField>,
+#[derive(Clone, Debug, PartialEq)]
+pub struct FiniteField<const P: u32, const N: u32> {
+    pub coords: [PrimeField<P>; N],
+    pub polynome: [PrimeField<P>; N],
 }
 
-impl Field for PrimeField {
+impl<const P:u32> Field for PrimeField<P> {
     // Neutral element for addition
-    fn zero() -> Self {
-        return PrimeField { order: Self::order(), value: BigInt::zero() }
+    fn zero(self) -> Self {
+        return PrimeField::<P> {value: BigInt::zero() }
     }
 
     // Neutral element for multiplication
-    fn one() -> Self {
-        return PrimeField {order: Self::order(), value: BigInt::one()};
+    fn one(self) -> Self {
+        return PrimeField::<P> {value: BigInt::one()};
     }
 
     // Addition
     fn add(self, y: &Self) -> Self {
         let res = self.value + y.value;
-        PrimeField {
-            order: self.order.clone(),
-            value: res.modulo(&self.order),
+        PrimeField::<P> {
+            value: res.modulo(&P),
         }
     }
 
     // Multiplication
     fn mul(self, y: &Self) -> Self {
         let res = self.value * y.value;
-        PrimeField { order: self.order.clone(), value: res.modulo(&self.order) }
+        PrimeField::<P> {value: res.modulo(&P) }
     }
 
     // Multiplication by an integer
-    fn zmul(self, y: i64) -> Self;
+    fn zmul(self, y: &i64) -> Self {
+        let res = self.value * y;
+        PrimeField::<P> {value: res.modulo(&P) }
+    }
 
     // Power
     fn pow(self, y: &BigInt) -> Self;
@@ -101,7 +104,9 @@ impl Field for PrimeField {
     fn zpow(self, y: i64) -> Self;
 
     // Division
-    fn div(self, y: &Self) -> Self;
+    fn div(self, y: &Self) -> Self {
+        return self.mul(&y.invert());
+    }
 
     // Squaring
     fn square(self) -> Self;
@@ -110,24 +115,105 @@ impl Field for PrimeField {
     fn sqrt(self) -> Self;
 
     // Multiplicative inverse
-    fn invert(self) -> Self;
+    fn invert(&self) -> Self;
 
     // Additive inverse
     fn neg(self) -> Self;
 
     // Degree of the extension
-    fn degree() -> Self {
+    fn degree() -> u32 {
         return 1;
     }
 
     // Field order
-    fn order(self) -> Self {
-        return self.order;
+    fn order(self) -> u32 {
+        P
     }
 
     // Base field order
-    fn base_order() -> BigInt;
+    fn base_order() -> BigInt {
+        P
+    }
 
     // Random field point
     fn random_element() -> Self;
+}
+
+impl<const P: u32, const N: u32> Field for FiniteField<P,N> {
+    // Neutral element for addition
+    fn zero(self) -> Self {
+        let zero = PrimeField { value: BigInt::zero() };
+        return FiniteField::<P,N> { coords: [zero; N], polynome: self.polynome }
+    }
+
+    // Neutral element for multiplication
+    fn one(self) -> Self {
+        let one = PrimeField { value: BigInt::one() };
+        return FiniteField::<P,N> { coords: [one; N], polynome: self.polynome }
+    }
+
+    fn add(self, y: &Self) -> Self {
+        let x = self.clone().coords;
+        let v = y.coords;
+        for i in 0..N {
+            x[i as usize] = x[i as usize].add(&v[i as usize]);
+        }
+        return FiniteField::<P,N> {coords: x, polynome: self.polynome};
+    }
+
+    fn mul(self, y: &Self) -> Self {
+        todo!()
+    }
+
+    fn zmul(self, y: &i64) -> Self {
+        let x = self.clone().coords;
+        for i in 0..N {
+            x[i as usize] = x[i as usize].zmul(y)
+        }
+        return FiniteField::<P,N> {coords: x, polynome: self.polynome};
+    }
+
+    fn pow(self, y: &BigInt) -> Self {
+        todo!()
+    }
+
+    fn zpow(self, y: i64) -> Self {
+        todo!()
+    }
+
+    fn div(self, y: &Self) -> Self {
+        todo!()
+    }
+
+    fn square(self) -> Self {
+        todo!()
+    }
+
+    fn sqrt(self) -> Self {
+        todo!()
+    }
+
+    fn invert(&self) -> Self {
+        todo!()
+    }
+
+    fn neg(self) -> Self {
+        todo!()
+    }
+
+    fn degree() -> u32 {
+        N
+    }
+
+    fn order(self) -> u32 {
+        todo!()
+    }
+
+    fn base_order() -> BigInt {
+        P
+    }
+
+    fn random_element() -> Self {
+        todo!()
+    }
 }
